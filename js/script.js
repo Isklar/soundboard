f2e66610ff0ebe30f6a7ad051cfc58b73c0901de
Isklar <mrisklar@gmail.com>
2016-02-22 01:14:29 +0000

    Show controls.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 	// Remove no-javascript class if js is enabled
 	$("body.no-javascript").removeClass("no-javascript")
 
-	$("audio").removeAttr("controls").each(function(i, audioElement) {
+	$("audio") {
 	    var audio = $(this);
 	    var that = this;
 	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
