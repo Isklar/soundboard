5bf297768bd85119ecd79093ebd0ca733996cc4e
Isklar <mrisklar@gmail.com>
2016-02-22 16:49:20 +0000

    This probably wont work.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-﻿$(document).ready(function(){
+﻿$(document).ready(function() {
 	// Remove no-javascript class if js is enabled
 	$("body.no-javascript").removeClass("no-javascript")
 
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button">'+audio.attr("title")+'</button> </a></li>').click(function() {
-	    	var player = document.getElementById(audio.attr("class"));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>'));
+	});
+
+}
+
+function play_pause(audioclass) {
+	    	var player = document.getElementById(audioclass);
 
 	    	if(!player){
 			    that.play();
@@ -20,7 +25,4 @@
 	    		player.pause();
 	    		player.currentTime = 0;
 	    	}
-	    }));
-	});
-
-});
+	    });
