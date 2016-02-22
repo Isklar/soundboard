7d897260956e7266884dd673920ffd794656dd74
Isklar <mrisklar@gmail.com>
2016-02-22 01:22:03 +0000

    Revert "Pause on click."
    This reverts commit c3124780caf0bb7520187e7a85221c2e77d66a3d.

--- js/script.js
+++ js/script.js
@@ -2,19 +2,10 @@
 	// Remove no-javascript class if js is enabled
 	$("body.no-javascript").removeClass("no-javascript")
 
-	$("audio").removeAttr("controls").each(function(i, audioElement) {
+	$("audio").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    var curPlaying;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {
-	        e.preventDefault();
-	        var song = $(this).next('audio')[0];
-	        if(song.paused){
-	            song.play();
-	            if(curPlaying) $("audio", "#"+curPlaying)[0].pause();
-	        } else { song.pause(); }
-	        curPlaying = $(this).parent()[0].id;
-	    });
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
 	});
 
 });
