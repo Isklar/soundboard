5514297d84684103ee7e4d9e6195235338ab3d83
Isklar <mrisklar@gmail.com>
2016-02-22 17:45:20 +0000

    Remove list.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button">'+audio.attr("title")+'</button> </a></li>').click(function() {
+	    $("#doc").append($('<a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button">'+audio.attr("title")+'</button> </a>').click(function() {
 			if(that.paused){
 	    		that.play()
 	    	}
