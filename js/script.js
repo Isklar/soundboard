bdbea3ff789f7a78e56286c374c5ba9c35182897
Isklar <mrisklar@gmail.com>
2016-02-22 17:19:15 +0000

    Remove the old stuff.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>').click(function() {
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button">'+audio.attr("title")+'</button> </a></li>').click(function() {
 			if(that.paused){
 	    		that.play()
 	    	}
@@ -17,15 +17,3 @@
 	});
 
 });
-
-function play_pause(audioclass) {
-	    	var player = document.getElementById(audioclass);
-
-	    	if(player.paused){
-	    		player.play()
-	    	}
-	    	else{
-	    		player.pause();
-	    		player.currentTime = 0;
-	    	}
-	    };
