005d7ebb7cd1795aa711b652004e0c1591996978
Isklar <mrisklar@gmail.com>
2016-02-22 01:33:30 +0000

    I know nothing about JS!

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@
 	    var audio = $(this);
 	    var that = this;
 	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {
-	    	if(that.paused){
-	    		that.play();
+	    	var player = document.getElementById(audio.attr("class"));
+	    	if(player.paused){
+	    		player.play();
 	    	}
 	    	else{
-	    		that.pause();
-	    		that.currentTime = 0;
+	    		player.pause();
+	    		player.currentTime = 0;
 	    	}
 	    }));
 	});
