1a0705fe688d4428011b887a4b6598e081ebda87
Isklar <mrisklar@gmail.com>
2016-02-22 16:20:58 +0000

    Testing button fix.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><button type="button">'+audio.attr("title")+'</button></a></li>').click(function() {
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"></a></li>').click(function() {
 	    	var player = document.getElementById(audio.attr("class"));
 
 	    	if(!player){
