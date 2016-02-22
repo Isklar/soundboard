c3124780caf0bb7520187e7a85221c2e77d66a3d
Isklar <mrisklar@gmail.com>
2016-02-22 01:22:03 +0000

    Pause on click.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@
 	// Remove no-javascript class if js is enabled
 	$("body.no-javascript").removeClass("no-javascript")
 
-	$("audio").each(function(i, audioElement) {
+	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
+	    var curPlaying;
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {
+	        e.preventDefault();
+	        var song = $(this).next('audio')[0];
+	        if(song.paused){
+	            song.play();
+	            if(curPlaying) $("audio", "#"+curPlaying)[0].pause();
+	        } else { song.pause(); }
+	        curPlaying = $(this).parent()[0].id;
+	    });
 	});
 
 });
