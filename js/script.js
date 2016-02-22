1607b9898da61c5c453753fa53204df624d7bfc6
Isklar <mrisklar@gmail.com>
2016-02-22 17:17:54 +0000

    Put it back similar to how it was.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>').click(function() {that.play();}));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>').click(function() {
+			if(that.paused){
+	    		that.play()
+	    	}
+	    	else{
+	    		that.pause();
+	    		that.currentTime = 0;
+	    	}
+	    }));
 	});
 
 });
@@ -14,7 +22,7 @@ function play_pause(audioclass) {
 	    	var player = document.getElementById(audioclass);
 
 	    	if(player.paused){
-	    		
+	    		player.play()
 	    	}
 	    	else{
 	    		player.pause();
