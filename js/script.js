b2a1b9acaad5e2dd640a95930b87941adc1f1a65
Isklar <mrisklar@gmail.com>
2016-02-22 01:58:55 +0000

    Copy paste error.

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@
 	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {
 	    	var player = document.getElementById(audio.attr("class"));
 
-	    	if(! variable_here){
-			    that.play()
+	    	if(!player){
+			    that.play();
+			    return;
 			};
 
 	    	if(player.paused){
