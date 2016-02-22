e8cfc965f59fd4687a1350c31723eae02c52bad2
Isklar <mrisklar@gmail.com>
2016-02-22 17:46:20 +0000

    It was better as a list.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button">'+audio.attr("title")+'</button> </a>').click(function() {
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> </a><button type="button">'+audio.attr("title")+'</button> </li>').click(function() {
 			if(that.paused){
 	    		that.play()
 	    	}
