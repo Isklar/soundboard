5661d16ba65333e4780f6fa37344ce72f1c5504c
Isklar <mrisklar@gmail.com>
2016-02-22 17:07:43 +0000

    Revert stuff.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"></a></li>'));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>'));
 	});
 
 });
