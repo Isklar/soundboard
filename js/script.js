e996fcd0bee80de4bda52c6e6e9532a06b0d9f27
Isklar <mrisklar@gmail.com>
2016-02-22 16:53:50 +0000

    Worth a try.

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>'));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"> <button type="button" onclick="play_pause('that+', '+audio.attr("class")+')">'+audio.attr("title")+'</button> </a></li>'));
 	});
 
 });
 
-function play_pause(audioclass) {
+function play_pause(that, audioclass) {
 	    	var player = document.getElementById(audioclass);
 
 	    	if(!player){
